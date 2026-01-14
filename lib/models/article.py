00f25983429c6aae6f51b2e09cc780abd14d84de
Bett007 <bettbrian022@gmail.com>
2026-01-14 20:55:56 +0300

    article.py update

--- lib/models/article.py
+++ lib/models/article.py
@@ -6,3 +6,39 @@ class Article:
         self.magazine = magazine
         self.title = title
         Article.all.append(self)
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # can't change after set once
+        if hasattr(self, "_title"):
+            return
+
+        if type(value) is not str:
+            raise TypeError("title must be a string")
+        if len(value) < 5 or len(value) > 50:
+            raise ValueError("title must be 5-50 characters")
+
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("author must be an Author instance")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise TypeError("magazine must be a Magazine instance")
+        self._magazine = value
