63f4d67714c700cb8b703e2a131aee4f5d34e3f1
Bett007 <bettbrian022@gmail.com>
2026-01-14 20:53:30 +0300

    add basic validations for author and magazine

--- lib/models/author.py
+++ lib/models/author.py
@@ -1,3 +1,20 @@
 class Author:
     def __init__(self, name):
         self.name = name
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # can't change after set once
+        if hasattr(self, "_name"):
+        
+            return
+
+        if type(value) is not str:
+            raise TypeError("name must be a string")
+        if len(value) == 0:
+            raise ValueError("name must be longer than 0 characters")
+
+        self._name = value
--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -2,3 +2,26 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if type(value) is not str:
+            raise TypeError("name must be a string")
+        if len(value) < 2 or len(value) > 16:
+            raise ValueError("name must be 2-16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if type(value) is not str:
+            raise TypeError("category must be a string")
+        if len(value) == 0:
+            raise ValueError("category must be longer than 0 characters")
+        self._category = value
