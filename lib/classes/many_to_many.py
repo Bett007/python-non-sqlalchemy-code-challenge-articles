894dd721a06a1ca02722c24023a65a3d3e7269f2
Bett007 <bettbrian022@gmail.com>
2026-01-14 21:57:31 +0300

    move models into many_to_many for tests

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,171 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # immutable after first set
+        if hasattr(self, "_name"):
+            return
+        if type(value) is not str:
+            return
+        if len(value) == 0:
+            return
+        self._name = value
+
     def articles(self):
-        pass
+        return [a for a in Article.all if a.author == self]
 
     def magazines(self):
-        pass
+        mags = []
+        for article in self.articles():
+            if article.magazine not in mags:
+                mags.append(article.magazine)
+        return mags
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if len(self.articles()) == 0:
+            return None
+        areas = []
+        for mag in self.magazines():
+            if mag.category not in areas:
+                areas.append(mag.category)
+        return areas
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if type(value) is not str:
+            return
+        if len(value) < 2 or len(value) > 16:
+            return
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if type(value) is not str:
+            return
+        if len(value) == 0:
+            return
+        self._category = value
 
     def articles(self):
-        pass
+        return [a for a in Article.all if a.magazine == self]
 
     def contributors(self):
-        pass
+        authors = []
+        for article in self.articles():
+            if article.author not in authors:
+                authors.append(article.author)
+        return authors
 
     def article_titles(self):
-        pass
+        arts = self.articles()
+        if len(arts) == 0:
+            return None
+        return [a.title for a in arts]
 
     def contributing_authors(self):
-        pass
+        arts = self.articles()
+        if len(arts) == 0:
+            return None
+
+        counts = {}
+        for a in arts:
+            counts[a.author] = counts.get(a.author, 0) + 1
+
+        result = []
+        for author, count in counts.items():
+            if count > 2:
+                result.append(author)
+
+        if len(result) == 0:
+            return None
+        return result
+
+    @classmethod
+    def top_publisher(cls):
+        if len(Article.all) == 0:
+            return None
+
+        counts = {}
+        for article in Article.all:
+            mag = article.magazine
+            counts[mag] = counts.get(mag, 0) + 1
+
+        top_mag = None
+        top_count = 0
+        for mag, count in counts.items():
+            if count > top_count:
+                top_mag = mag
+                top_count = count
+
+        return top_mag
+
+
+class Article:
+    all = []
+
+    def __init__(self, author, magazine, title):
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # immutable after first set
+        if hasattr(self, "_title"):
+            return
+        if type(value) is not str:
+            return
+        if len(value) < 5 or len(value) > 50:
+            return
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            return
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            return
+        self._magazine = value
